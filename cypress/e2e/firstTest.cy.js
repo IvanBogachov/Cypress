c736c95924647465822ac1dad3b0a413f4796efd
IvanBogachov <106016809+IvanBogachov@users.noreply.github.com>
2025-07-01 21:24:32 +0300

    add new test

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -9,4 +9,27 @@ describe('Наш перший блок тестів', () => {
     //знайде кнопку Log in та запустить подію click
     cy.get('button[type="submit"]').click();
   });
+
+  it('Перевірка вмісту і кольору кнопки Log in', () => {
+    // Тест відвідування сайту LMS
+    cy.visit('https://www.edu.goit.global/account/login');
+    //Знайти на ній кнопку логінізації Перевірити чи кнопка містить всередині себе текст Log in
+    cy.get('[type="submit"]').should('have.text', 'Log in');
+    //чи має кнопка колір фону rgb(255, 107, 10)
+    cy.get('[type="submit"]').should(
+      'have.css',
+      'background-color',
+      'rgb(255, 107, 10)'
+    );
+  });
+
+  it('scrollIntoView test кнопки Go to the course', () => {
+    // Тест відвідування сайту LMS
+    cy.visit('https://www.edu.goit.global/account/login');
+    cy.login('user888@gmail.com', '1234567890');
+    //скролимо до кнопки і перевіряємо чи вона відображається
+    cy.get('#go-to-the-course-homepage-widget')
+      .scrollIntoView()
+      .should('be.visible');
+  });
 });
